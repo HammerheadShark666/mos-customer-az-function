53a2e902e578f85ee4cdff11b7c0da60d981203e
John Miller <john.miller@unionstreet.co.uk>
2024-09-08 15:19:28 +0100

    Change service bus connection for using with managed identity

--- Microservice.Customer.Function/Helpers/Constants.cs
+++ Microservice.Customer.Function/Helpers/Constants.cs
@@ -4,7 +4,7 @@ public class Constants
 {
     public const string DatabaseConnectionString = "SQLAZURECONNSTR_CUSTOMER";
 
-    public const string AzureServiceBusConnection = "AZURE_SERVICE_BUS_CONNECTION";
+    public const string AzureServiceBusConnection = "ServiceBusConnection";
     public const string AzureServiceBusQueueRegisteredUserCustomer = "AZURE_SERVICE_BUS_QUEUE_CUSTOMER";
 
     public const string FailureReasonValidation = "Validation Errors.";
