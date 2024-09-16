abdbf92143a1f4f883cc16db500cb0625da1104f
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2024-09-16 10:54:35 +0100

    Clean up

--- Microservice.Customer.Function/Helpers/Constants.cs
+++ Microservice.Customer.Function/Helpers/Constants.cs
@@ -2,10 +2,7 @@
 
 public class Constants
 {
-    //public const string DatabaseConnectionString = "SQLAZURECONNSTR_CUSTOMER";
-
     public const string AzureServiceBusConnectionManagedIdentity = "ServiceBusConnection";
-    //public const string AzureServiceBusConnection = "ServiceBusConnection";
     public const string AzureServiceBusQueueRegisteredUserCustomer = "AZURE_SERVICE_BUS_QUEUE_CUSTOMER";
 
     public const string FailureReasonValidation = "Validation Errors.";
